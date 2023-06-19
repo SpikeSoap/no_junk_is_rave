d4e264b6a437dc3d474bfe725bac8ff4863811b0
Soap <gorka.jabonero@estudioenpenascal.com>
2023-06-19 14:22:09 +0200

    Audio resulto

--- script/cancion.js
+++ script/cancion.js
@@ -6,91 +6,75 @@ const albumes = [
         titulo: "Morphine - Have a lucky day",
         caratula: "../images/Caratula1.png",
         descripcion: "Morphine de Have a Lucky Day es una canción con un ritmo hipnotizante y letras introspectivas que te sumergen en una atmósfera melancólica y adictiva.",
-        cancion: "audio/cancion1.mp3",
+        cancion: "../audio/cancion1.wav",
     },
     {
         id: 2,
         titulo: "Herencia de timbiquí - Sabrás",
         caratula: " ../images/Caratula2.png",
         descripcion: "Herencia de Timbiquí es una reconocida agrupación musical colombiana que fusiona ritmos afrocolombianos, como el currulao y la marimba, con elementos modernos.",
-        cancion: "audio/cancion2.mp3",
+        cancion: "../audio/cancion2.mp3",
     },
     {
         id: 3,
         titulo: "Trío Matamoros - Lágrias negras",
         caratula: "../images/Caratula3.png",
         descripcion: "El Trío Matamoros es una destacada agrupación musical cubana. Lágrimas negras es un clásico de su repertorio, una emotiva canción que expresa el dolor.",
-        cancion: "audio/cancion3.mp3",
+        cancion: "../audio/cancion3.mp3",
     },
     {
         id: 4,
         titulo: "Erikah Badu - On & On",
         caratula: "../images/Caratula4.png",
         descripcion: "Erykah Badu es un icónico tema de neo soul que captura la esencia del estilo y la personalidad de la artista. Con su voz seductora y letras introspectivas.",
-        cancion: "audio/cancion4.mp3",
+        cancion: "../audio/cancion4.mp3",
     },
     {
         id: 5,
         titulo: "Johnny Hallyday - Marie",
         caratula: "../images/Caratula5.png",
         descripcion: " Con su voz poderosa y emotiva, Hallyday expresa la pasión y el deseo por su amada Marie. La canción evoca una mezcla de romanticismo y nostalgia.",
-        cancion: "audio/cancion5.mp3",
+        cancion: "../audio/cancion5.mp3",
     },
     {
         id: 6,
         titulo: "Bob Dylan - Saved",
         caratula: "../images/Caratula6.png",
         descripcion: "Con una fusión de rock y gospel, Dylan explora su fe y el concepto de salvación. La canción transmite una energía espiritual y presenta letras poderosas que reflejan la evolución.",
-        cancion: "audio/cancion6.mp3",
+        cancion: "../audio/cancion6.mp3",
     },
     {
         id: 7,
         titulo: "BB & The HC - Piece of My Heart",
         caratula: "../images/Caratula7.png",
         descripcion: "Es una canción icónica de rock que presenta la poderosa voz de Janis Joplin. Con su intensidad y emoción desgarradora, transmite el anhelo.",
-        cancion: "audio/cancion7.mp3",
+        cancion: "../audio/cancion7.mp3",
     },
     {
         id: 8,
         titulo: "Esne Beltza - Esnesaltzailearena ",
         caratula: "../images/Caratula8.png",
         descripcion: "Es una canción enérgica y festiva que combina elementos del rock, ska y música tradicional vasca. Con su ritmo contagioso y letras enérgicas, invita a bailar.",
-        cancion: "audio/cancion8.mp3",
+        cancion: "../audio/cancion8.mp3",
     },
     {
         id: 9,
         titulo: "Melendi - Un violinista en tu tejado",
         caratula: "../images/Caratula9.png",
         descripcion: "Es una canción romántica que narra la historia de un amor clandestino y apasionado. Con su estilo pop-rock y letras poéticas, Melendi evoca la emoción.",
-        cancion: "audio/cancion9.mp3",
+        cancion: "../audio/cancion9.mp3",
     },
     {
         id: 10,
         titulo: "CCR - Have You Ever Seen The Rain",
         caratula: "../images/Caratula10.png",
         descripcion: "Con su melodía pegadiza y letras introspectivas, la canción reflexiona sobre la ilusión y la desilusión en la vida.",
-        cancion: "audio/cancion10.mp3",
+        cancion: "../audio/cancion10.mp3",
     },
 ];
 
-
-
-
-
-// 
-
-
-
-
-
-// 
-
-
 // Creación de las tarjetas 
-
-
 const cards = document.querySelector(".cards");
-
 
 for (let i = 0; i < albumes.length; i++) {
     let card = document.createElement("label");
@@ -98,7 +82,6 @@ for (let i = 0; i < albumes.length; i++) {
     card.id = "song-" + (i + 1);
     card.setAttribute("for", "item-" + (i + 1));
     card.classList.add("art-cancion");
-    console.log( card.id);
     cards.appendChild(card);
 
     let titulo = document.createElement("h3");
@@ -112,23 +95,57 @@ for (let i = 0; i < albumes.length; i++) {
     cardCont.classList.add("cancion-con");
     card.appendChild(cardCont);
 
+    let cardDes = document.createElement("div");
+    cardDes.className = "card-desc" + i;
+    cardDes.classList.add("card-des");
+    cardCont.appendChild(cardDes);
+
     let texto = document.createElement("p");
     texto.className = "texto" + i;
     texto.textContent = albumes[i].descripcion;
     texto.classList.add("cancion-des");
-    cardCont.appendChild(texto);
+    cardDes.appendChild(texto);
 
+    // Imagen play
+    let btnPlay = document.createElement("img");
+    btnPlay.className = "btn-play";
+    btnPlay.id= i + 1;
+    btnPlay.classList.add("btn-play");
+    btnPlay.src= "../images/play.png";
+    cardDes.appendChild(btnPlay);
+
+    // Imagen carátula
     let imagen = document.createElement("img");
     imagen.className = "imagen" + i;
     imagen.src = albumes[i].caratula;
     imagen.classList.add("cancion-img");
     cardCont.appendChild(imagen);
 
-    console.log(card.className);
+    
 }
 
+const reproducir = document.querySelectorAll(".btn-play");
+const titleCan = document.querySelector(".title-cancion");
+const caraTula = document.querySelector(".caratula");
 
 
-// $('input').on('change', function() {
-//     $('body').toggleClass('blue');
-//   });
+for(let i = 0; i<reproducir.length; i++ ){
+
+ reproducir[i].addEventListener("click", (e)=>{
+    let play = e.target.id;
+
+    if(play== albumes[i].id){
+       let cancion = new Audio("../audio/cancion" + (i+1)  + ".mp3");
+
+       cancion.play();
+
+        titleCan.textContent = albumes[i].titulo;
+        // for(let a = 0; a <caraTula.length; a++){
+
+        //     caraTula.src = albumes[i].caratula;
+        // }
+        caraTula.src = albumes[i].caratula;
+        caraTula.classList.add("caratula");
+    }
+ });   
+};
